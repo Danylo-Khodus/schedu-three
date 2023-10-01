b7a039c8c250e0f9a71131c9b2119e4a748e763b
Danylo-Khodus <134836895+Danylo-Khodus@users.noreply.github.com>
2023-10-01 18:50:45 +0300

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
         if (requirements) {
             ev.preventDefault();
 
-            const response = await fetch('https://schedu-three.vercel.app/' + '/login', {
+            const response = await fetch('https://schedu-three.vercel.app/api' + '/login', {
                 method: 'POST',
                 body: JSON.stringify(userData),
                 headers: {'Content-Type':'application/json'},
