9323736dc0f44cf71d97f104870cf172c8da116e
Danylo-Khodus <danilhodusagario@gmail.com>
2023-10-23 11:29:35 +0300

    2023-10-23 11:29

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ export default function HomePage() {
 
     const [first] = lessons;
 
-    const lastLesson = () => {if (lessons.length > 1) {lessons.pop()}};
+    const lastLesson = lessons.pop();
 
     // DISPLAYING LESSON INFO
 
